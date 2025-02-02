db6c022a7015eb71eb935f0baf41d25189ba1b19
Julius Meriläinen <34103045+devjume@users.noreply.github.com>
2025-02-02 06:11:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,6 @@
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
-sleep 3
-
-echo "dog food"
-echo $HELLO
-echo ${HELLO}
-echo $TAILSCALE_AUTHKEY
-
 _term() {
     echo "Caught SIGTERM signal. Logging out and cleaning up."
     trap - TERM
@@ -23,10 +16,8 @@ trap _term TERM
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 TAILSCALE_DAEMON_PID=$!
 
-sleep 3
-
-/app/tailscale up --ssh --authkey="${TAILSCALE_AUTHKEY}" --hostname="${KOYEB_APP_NAME}" --advertise-routes="10.2.208.6/32"
-#/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-exit-node
+#/app/tailscale up --ssh --authkey="${TAILSCALE_AUTHKEY}" --hostname="${KOYEB_APP_NAME}" --advertise-routes="10.2.208.6/32"
+/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-exit-node
 #/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${HOSTNAME}
 
 wait
