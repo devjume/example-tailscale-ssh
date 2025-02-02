97c8eb1c3fcc436b066c7b0de8fec01e0a2d6b7b
Julius Meriläinen <34103045+devjume@users.noreply.github.com>
2025-02-02 04:36:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ trap _term TERM
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 TAILSCALE_DAEMON_PID=$!
-/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-routes="0.0.0.0/0,::/0"
-# /app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-exit-node
+#/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-routes="0.0.0.0/0,::/0"
+/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${KOYEB_APP_NAME} --advertise-exit-node
 #/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${HOSTNAME}
 
 wait
