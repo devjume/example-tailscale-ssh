f3f21510ded63cbe0c139dd4b28fa01ee18108bd
Julius Meriläinen <34103045+devjume@users.noreply.github.com>
2025-02-12 01:36:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,10 +16,15 @@ trap _term TERM
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 TAILSCALE_DAEMON_PID=$!
 
-#/app/tailscale up --ssh --authkey="${TAILSCALE_AUTHKEY}" --hostname="${KOYEB_APP_NAME}" --advertise-routes="10.2.208.6/32"
+# /app/tailscale up --ssh --authkey="${TAILSCALE_AUTHKEY}" --hostname="${KOYEB_APP_NAME}" --advertise-routes="10.2.208.6/32" --accept-dns=true
 #/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}" --advertise-exit-node
-#/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}" --advertise-exit-node --accept-dns=false
-/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}" --advertise-routes=10.3.0.0/24 --accept-dns=false 
+#/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}" --advertise-exit-node --accept-dns=true
+
+#https://tailscale.com/kb/1364/serverless
+#/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+#/app/tailscale up -ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}"
+
+/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname="koyeb-${KOYEB_APP_NAME}-${KOYEB_SERVICE_NAME}" --advertise-routes=10.3.0.0/24 --accept-dns=true 
 #/app/tailscale up --ssh --authkey=${TAILSCALE_AUTHKEY} --hostname=${HOSTNAME}
 
 wait
